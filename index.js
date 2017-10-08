6f13c90738fe6523fce27629026170ad8af08e93
Tommy Brettschneider <tommy.brettschneider@gmail.com>
2017-10-08 22:42:31 +0200

    add bunyan logging, eslint config

--- index.js
+++ index.js
@@ -1,64 +1,70 @@
-"use strict";
-
-var restify = require('restify'),
-    errors  = require('restify-errors'),
+let restify = require('restify'),
     osmosis = require('osmosis'),
+    bunyan = require('bunyan'),
+    log = bunyan.createLogger({
+        name: 'audit',
+        stream: process.stdout
+    }),
     server = restify.createServer({handleUncaughtExceptions: true});
 
-server.use(restify.plugins.bodyParser({
-    requestBodyOnGet: true
+server.on('after', restify.plugins.auditLogger({
+    log: log,
+    event: 'after',
+    server: server
 }));
 
-server.pre(function(req, res, next) {
-    console.info('before route is determined!');
-    return next();
-});
-
-server.use(function(req, res, next) {
-    console.info('run for all routes!');
-    return next();
-});
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.bodyParser({requestBodyOnGet: true}));
+server.use(restify.plugins.jsonp());
+server.use(restify.plugins.gzipResponse());
+server.use(restify.plugins.authorizationParser());
 
 server.post('/scraper', (req, res, next) => {
-    handleRequest(req, res, next);
-})
-
-function handleRequest(req, res, next) {
-    console.info('handle incoming request');
-
-    var config = JSON.parse(req.body);
-    var url = config.url;
-    var selectors = config.selectors,
-        opengraph = config.opengraph;
+    log.info('handle incoming request');
+    let reqBody = JSON.parse(req.body);
     osmosis
-        .get(url)
+        .get(reqBody.url)
         .set({
             title: 'title',
             description: 'meta[name="description"]@content',
-            queryResults: osmosis.set(selectors),
-            'opengraph': osmosis.set({
-                'og:url': 'meta[property="og:url"]@content',
-                'og:type': 'meta[property="og:type"]@content',
-                'og:title': 'meta[property="og:title"]@content',
-                'og:site_name': 'meta[property="og:site_name"]@content',
-                'og:description': 'meta[property="og:description"]@content',
-                'og:images': osmosis.set([
-                    {
-                        'og:image':'meta[property"og:image"]@content',
-                        'og:image:type': 'meta[property="og:image:type"]@content',
-                        'og:image:width': 'meta[property="og:image:width"]@content',
-                        'og:image:height': 'meta[property="og:image:height"]@content'
-                    }
-                ])
-            })
+            queryResults: osmosis.set(reqBody.selectors)
         })
-        .data(function(result) {
-            result.url = url;
-            res.send(result);
-            return next();
+        .then(function(context, data, next, done) {
+            if (reqBody.opengraph) {
+                log.info('get opengraph data');
+            }
+            next(context, data);
         })
-}
+        .then(function(context, data, next, done) {
+            data.url = context.request.href;
+            res.send(data);
+            done();
+        })
+        .log(log);
+});
 
 server.listen(8080, function() {
-    console.log('%s listening at %s', server.name, server.url);
+    log.info('%s listening at %s', server.name, server.url)
 });
+
+/*
+if (opengraph) {
+    osmosis.data({
+        'opengraph': osmosis.set({
+            'og:url': 'meta[property="og:url"]@content',
+            'og:type': 'meta[property="og:type"]@content',
+            'og:title': 'meta[property="og:title"]@content',
+            'og:site_name': 'meta[property="og:site_name"]@content',
+            'og:description': 'meta[property="og:description"]@content',
+            'og:images': osmosis.set([
+                {
+                    'og:image': 'meta[property"og:image"]@content',
+                    'og:image:type': 'meta[property="og:image:type"]@content',
+                    'og:image:width': 'meta[property="og:image:width"]@content',
+                    'og:image:height': 'meta[property="og:image:height"]@content'
+                }
+            ])
+        })
+    })
+}
+*/
